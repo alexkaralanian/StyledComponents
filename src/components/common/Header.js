297a8d8c3802201a2c30109d0055974f3c2468ed
Alex Karalanian <alex.karalanian@gmail.com>
2020-05-15 16:10:39 -0400

    styled link and implemented isActive on nav

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 const HeaderWrapper = styled.div`
   height: 60px;
@@ -13,11 +14,45 @@ const HeaderWrapper = styled.div`
   background: #eee;
 `;
 
+const Menu = styled.nav`
+  display: flex;
+  position: relative;
+  width: initial;
+  border-bottom: none;
+  margin: auto 0 auto auto;
+  font-family: 'Open Sans';
+  background: none;
+  left: initial;
+  top: initial;
+`;
+
+// Here we import Link from RR as RouterLInk
+// We need to destructure the isActive Prop in order to pass it down to the StyledLink component correctly
+const Link = ({ isActive, children, ...props }) => {
+  return <RouterLink {...props}>{children}</RouterLink>;
+};
+
+const StyledLink = styled(Link)`
+  padding: 4px 8px;
+  display: block;
+  text-align: center;
+  box-sizing: border-box;
+  margin: auto 0;
+  font-weight: ${(props) => (props.isActive ? 'bold' : 'normal')};
+`;
+
 export function Header() {
+  const { pathname } = useLocation();
   return (
     <HeaderWrapper>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login</Link>
+      <Menu>
+        <StyledLink isActive={pathname === '/'} to="/">
+          Home
+        </StyledLink>
+        <StyledLink isActive={pathname === '/login'} to="/login">
+          Login
+        </StyledLink>
+      </Menu>
     </HeaderWrapper>
   );
 }
